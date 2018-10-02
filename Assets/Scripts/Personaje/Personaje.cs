87fcbaf25dd27c503639cf261d9a71037f4a8f4f
juniniv3 <juniniprogramador@outlook.com>
2018-10-02 14:34:42 -0500

    Agregadas nuevas clases al proyecto
    Se agregan nuevas clases

--- Assets/Scripts/Personaje/Personaje.cs
+++ Assets/Scripts/Personaje/Personaje.cs
@@ -8,16 +8,61 @@ public class Personaje : MonoBehaviour {
     private int id;
     private string nombre;
     private string estado;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public string Nombre
+    {
+        get
+        {
+            return nombre;
+        }
+
+        set
+        {
+            nombre = value;
+        }
+    }
+
+    public string Estado
+    {
+        get
+        {
+            return estado;
+        }
+
+        set
+        {
+            estado = value;
+        }
+    }
+
+
+
    
-
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
---
+++ Assets/Scripts/Usuario/Usuario.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Usuario : MonoBehaviour {
+
+    //atributos - propiedades de la clase usuario
+
+    private int id;
+    private string email;
+    private string contraseña;
+    private string estado;
+    private string nombre;
+    // usuario que se usarà para gestionar la cuenta
+    private string usuario;
+    private DateTime fechaRegistro;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public bool login()
+    {
+        //función de loggin
+        return true;
+
+    }
+
+    public bool logout()
+    {
+        // code función de logout
+        return true;
+    }
+
+    public bool cambiarProyecto (){
+        //code función cambio de correo
+
+        return true;
+    }
+
+    public bool recuperarContraseña (){
+        return true;
+    }
+
+
+
+}
