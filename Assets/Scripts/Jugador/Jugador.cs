e952b5c4c4016ef1c2a7ad30fb3f933a52febc5d
juniniv3 <juniniprogramador@outlook.com>
2018-10-02 13:50:34 -0500

    Agregados algunos scripts de clases
    Se agregan algunos scripts de las clases planteadas en el diagrama de clases y se avanza en el desarrollo de los niveles

---
+++ Assets/Scripts/Jugador/Jugador.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour {
+
+
+    //atributos - propiedades
+    private int id;
+    private int salud;
+    private string estado;
+    private Personaje personaje;
+    private float poder;
+    private float velocidadMovimiento;
+    private float velocidadAtaque;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public int Salud
+    {
+        get
+        {
+            return salud;
+        }
+
+        set
+        {
+            salud = value;
+        }
+    }
+
+    public string Estado
+    {
+        get
+        {
+            return estado;
+        }
+
+        set
+        {
+            estado = value;
+        }
+    }
+
+    public Personaje Personaje
+    {
+        get
+        {
+            return personaje;
+        }
+
+        set
+        {
+            personaje = value;
+        }
+    }
+
+    public float Poder
+    {
+        get
+        {
+            return poder;
+        }
+
+        set
+        {
+            poder = value;
+        }
+    }
+
+    public float VelocidadMovimiento
+    {
+        get
+        {
+            return velocidadMovimiento;
+        }
+
+        set
+        {
+            velocidadMovimiento = value;
+        }
+    }
+
+    public float VelocidadAtaque
+    {
+        get
+        {
+            return velocidadAtaque;
+        }
+
+        set
+        {
+            velocidadAtaque = value;
+        }
+    }
+
+
+
+   
+}
---
+++ Assets/Scripts/Personaje/Personaje.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personaje : MonoBehaviour {
+
+    //atributos - propiedades de la clase personaje
+    private int id;
+    private string nombre;
+    private string estado;
+   
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
