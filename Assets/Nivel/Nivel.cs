bf27e61fd3ec2ada21e66035f47d4f4535d1cef1
juniniv3 <juniniprogramador@outlook.com>
2018-10-02 15:00:37 -0500

    Agregado mas scripts al proyecto
    Agregado mas scripts al proyecto

---
+++ Assets/Nivel/Nivel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nivel : MonoBehaviour {
+
+
+    //atributos - propiedades de la clase nivel
+    private int id;
+    private string nivelDificultad;
+    private string listaEnemigos;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public string NivelDificultad
+    {
+        get
+        {
+            return nivelDificultad;
+        }
+
+        set
+        {
+            nivelDificultad = value;
+        }
+    }
+
+
+  
+}
--- Assets/Scripts/Enemigo/Enemigo.cs
+++ Assets/Scripts/Enemigo/Enemigo.cs
@@ -4,13 +4,80 @@ using UnityEngine;
 
 public class Enemigo : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //atributos - propiedades de la clase Enemigo
+    private int id;
+    private string nombre;
+    // private Sprite sprite;
+    private int daño;
+    private float delay;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public string Nombre
+    {
+        get
+        {
+            return nombre;
+        }
+
+        set
+        {
+            nombre = value;
+        }
+    }
+
+    public int Daño
+    {
+        get
+        {
+            return daño;
+        }
+
+        set
+        {
+            daño = value;
+        }
+    }
+
+    public float Delay
+    {
+        get
+        {
+            return delay;
+        }
+
+        set
+        {
+            delay = value;
+        }
+    }
+
+
+
+  
 }
