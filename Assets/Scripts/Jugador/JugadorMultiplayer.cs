84ec093b5798a9b22c8cffb541e00a1898578890
Yisus Florez <juniniprogramador@outlook.com>
2018-10-13 17:09:37 -0500

    Avance en el networking LAN
    se implementa algunas funciones a las clases que controlan el sistema lan

---
+++ Assets/Scripts/Jugador/JugadorMultiplayer.cs
@@ -0,0 +1,228 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class JugadorMultiplayer : NetworkBehaviour {
+
+
+    //atributos - propiedades
+    private int id;
+    private int salud;
+    private string estado;
+    private Personaje personaje;
+    private float poder;
+    private float velocidadMovimiento = 100f;
+    private float velocidadAtaque;
+    private bool tocandoPared = false;
+    private bool tocandoItem = false;
+
+   
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+
+
+    void FixedUpdate()
+    {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        actualizarDireccionSprite();
+
+        //mantener velocidad máxima
+        if (rb.velocity.magnitude > 100f)
+        {
+            rb.velocity = rb.velocity.normalized * 100f;
+        }
+
+
+        anim.SetFloat("velocidad", Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("velocidadVertical", Mathf.Abs(rb.velocity.y));
+
+        //variable que toma los inputs horizontales "S", "W" o las flechas derecha, izquierda
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector2 movimiento = new Vector2(moveHorizontal, 0);
+
+        //agregando el movimiento 
+        rb.AddForce(movimiento * velocidadMovimiento);
+
+
+
+        if (Input.GetKeyDown("space"))
+        {
+            saltar();
+        }
+
+       
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            recogerTirar();
+        }
+
+
+    }
+
+    //funcion de salto
+    void saltar() {
+
+        if (tocandoPared)
+        {
+            Vector2 salto = new Vector2(0, 35);
+            rb.AddForce(salto * velocidadMovimiento);
+        }
+
+    }
+
+    public void recogerTirar() {
+
+        anim.SetBool("atacando", true);
+
+    }
+
+    
+    //funcion actualizar dirección sprite
+    public void actualizarDireccionSprite(){
+
+        //actualizar dirección del sprite si se está moviendo hacia la derecha
+        if (rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+
+        }
+
+        // cambiar dirección del sprite si se está moviendo hacia la izquierda
+        if(rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+           
+        }
+    }
+
+    //funcion para obtener recursos
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        this.tocandoPared = true;
+    }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        this.tocandoPared = false;
+    }
+
+
+
+
+
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public int Salud
+    {
+        get
+        {
+            return salud;
+        }
+
+        set
+        {
+            salud = value;
+        }
+    }
+
+    public string Estado
+    {
+        get
+        {
+            return estado;
+        }
+
+        set
+        {
+            estado = value;
+        }
+    }
+
+    public Personaje Personaje
+    {
+        get
+        {
+            return personaje;
+        }
+
+        set
+        {
+            personaje = value;
+        }
+    }
+
+    public float Poder
+    {
+        get
+        {
+            return poder;
+        }
+
+        set
+        {
+            poder = value;
+        }
+    }
+
+    public float VelocidadMovimiento
+    {
+        get
+        {
+            return velocidadMovimiento;
+        }
+
+        set
+        {
+            velocidadMovimiento = value;
+        }
+    }
+
+    public float VelocidadAtaque
+    {
+        get
+        {
+            return velocidadAtaque;
+        }
+
+        set
+        {
+            velocidadAtaque = value;
+        }
+    }
+
+
+
+   
+}
---
+++ Assets/Scripts/Personaje/Personaje Multiplayer.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeMultiplayer : MonoBehaviour {
+
+    //atributos - propiedades de la clase personaje
+    private int id;
+    private string nombre;
+    private string estado;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public string Nombre
+    {
+        get
+        {
+            return nombre;
+        }
+
+        set
+        {
+            nombre = value;
+        }
+    }
+
+    public string Estado
+    {
+        get
+        {
+            return estado;
+        }
+
+        set
+        {
+            estado = value;
+        }
+    }
+
+
+   
+   
+}
