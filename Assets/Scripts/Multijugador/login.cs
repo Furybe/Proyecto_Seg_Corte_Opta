f2068b6b9ca3e4577ae3602ddaf12fdb29585bca
juniniv3 <juniniprogramador@outlook.com>
2018-10-15 17:13:22 -0500

    Arreglando nivel 1

---
+++ Assets/Scripts/Multijugador/login.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class web : MonoBehaviour {
+
+    [SerializeField]
+    private InputField usuario;
+
+    [SerializeField]
+    private InputField contraseña;
+
+    // Use this for initialization
+    void Start() {
+
+        StartCoroutine(GetText());
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    IEnumerator GetText()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get("http://localhost:8000/api/usuarios"))
+        {
+            yield return www.Send();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                // Show results as text
+                Debug.Log(www.downloadHandler.text);
+
+                // Or retrieve results as binary data
+                byte[] results = www.downloadHandler.data;
+            }
+        }
+    
+}
+}
