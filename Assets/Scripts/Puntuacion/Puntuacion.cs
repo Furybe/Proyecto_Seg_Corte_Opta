43f5042f8747819a85cd24d9ae92ae6341444713
JhoanRc <jreyes549@unab.edu.co>
2018-10-16 11:22:33 -0500

    Implementado el tiempo de juego en los niveles.
    Implementación del tiempo de juego en los tres niveles. pruebas con la zona.

--- Assets/Scripts/Puntuacion/Puntuacion.cs
+++ Assets/Scripts/Puntuacion/Puntuacion.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Puntuacion : MonoBehaviour {
 
+	//atributos - propiedades
+	private double puntos;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +15,10 @@ public class Puntuacion : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+	// funcion que se encarga de sumar los puntos obtenidos en la partida.
+	// 1000 puntos por ganar, 100 puntos por matar a otro jugador.
+	private void puntuacion(){
+
 	}
 }
---
+++ Assets/Scripts/Zona/ControladorZona.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControladorZona : MonoBehaviour {
+
+	public RectTransform asd;
+	private string timer;
+	// Use this for initialization
+	void Awake () {
+		timer = ""+ Time.timeSinceLevelLoad.ToString("f0");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(timer);
+		if (timer == "5" || timer == "20" || timer == "25")
+		{
+			zona();
+		}
+	}
+	private void zona(){
+
+		asd.transform.localScale = new Vector3(-20f,0,0);
+	}
+}
