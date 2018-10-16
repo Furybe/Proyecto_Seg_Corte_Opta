3db93514ba8ca33c278b56341d6c8269545be620
juniniv3 <juniniprogramador@outlook.com>
2018-10-16 09:42:47 -0500

    Se implementan nuevas funcionalidades en la clase enemigo

--- Assets/Scripts/Enemigo/Enemigo.cs
+++ Assets/Scripts/Enemigo/Enemigo.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class Enemigo : MonoBehaviour {
 
     //atributos - propiedades de la clase Enemigo
+    // id para identificar los distintos tipos de enemigos
     private int id;
+   
+    //nombre para identificar el objeto en juego
     private string nombre;
+
     // private Sprite sprite;
     private int daño;
+
+    //variable usada para representar el tiempo de espera para lanzar una habilidad de nuevo
     private float delay;
 
+    //salud
+    private int salud;
 
     // Use this for initialization
     void Start()
@@ -24,7 +32,17 @@ public class Enemigo : MonoBehaviour {
 
     }
 
-    
+    //funcion atacar, vacía ya que se sobrescribe en cada una de las clases hijas
+    void atacar()
+    {
+
+    }
+
+    //funcion activada se recibe daño del ataque de un jugador
+    void recibirDaño()
+    {
+
+    }
 
     public int Id
     {
