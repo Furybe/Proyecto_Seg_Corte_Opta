dff47c9c9f13d90623c413c58106270906b224d1
Yisus Florez <juniniprogramador@outlook.com>
2018-10-19 22:43:00 -0500

    Agregada animacion de farmeo al arbol

--- Assets/Scripts/Recurso/Arbol.cs
+++ Assets/Scripts/Recurso/Arbol.cs
@@ -2,28 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Roca : Recurso {
+public class Arbol : Recurso {
 
     [SerializeField]
     private GameObject martillito;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
         this.martillito.SetActive(false);
-       
-       
-            
+
+
+
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnTriggerStay2D(Collider2D collider)
     {
         //activamos el martillo para que se muestre en la scene que es un objeto Farmeable
-        if (collider.tag=="jugador")
+        if (collider.tag == "jugador")
         {
             this.martillito.SetActive(true);
 
@@ -33,12 +35,11 @@ public class Roca : Recurso {
     void OnTriggerExit2D(Collider2D collider)
     {
         //desactivamos el martillo cuando el jugador se aleje de la piedra
-        if (collider.tag=="jugador")
+        if (collider.tag == "jugador")
         {
             martillito.SetActive(false);
         }
-        
-    }
 
+    }
 
 }
