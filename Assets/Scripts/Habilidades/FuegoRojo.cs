d094059fb0420b6e86dc760f14bc4902c0ff3367
Yisus Florez <juniniprogramador@outlook.com>
2018-10-22 14:19:30 -0500

    corrección fuego rojo

--- Assets/Scripts/Habilidades/FuegoRojo.cs
+++ Assets/Scripts/Habilidades/FuegoRojo.cs
@@ -6,11 +6,14 @@ public class FuegoRojo : Habilidad {
 
     [SerializeField]
     private Rigidbody2D rb;
-
+    private float velocidadFuego;
 
 	// Use this for initialization
 	void Start () {
+
+        //inicializando propiedades
         this.Daño = 20;
+        this.velocidadFuego = 6000f;
         rb = GetComponent<Rigidbody2D>();
     }
 	
@@ -29,13 +32,13 @@ public class FuegoRojo : Habilidad {
         this.IdEjecutor = idJugador;
         if (direccion==1)
         {
-            rb.AddForce(transform.right * 4000f);
+            rb.AddForce(transform.right * 6000f);
 
         }
         else
         {
             //agregamos la fuerza al gameobject
-            rb.AddForce(-transform.right* 4000f);
+            rb.AddForce(-transform.right* 6000f);
             
             //giramos el gameObject para que la animación se vea correcta
             transform.localScale=new Vector3(-1, 1, 1);
@@ -51,9 +54,17 @@ public class FuegoRojo : Habilidad {
             {
 
             }
+            else
+            {
+                
+            }
+
+        }
+        else
+        {
             
         }
-        //Destroy(this.gameObject);
+       
     }
 
 }
