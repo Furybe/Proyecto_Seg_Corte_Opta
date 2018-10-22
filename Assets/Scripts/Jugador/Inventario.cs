2a865d8e76c055866c8aede5addd3a0753cf6e87
Yisus Florez <juniniprogramador@outlook.com>
2018-10-22 10:27:23 -0500

    Avance lógica items

---
+++ Assets/Scripts/Jugador/Inventario.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventario : MonoBehaviour {
+
+    private List<PocionRoja> pocionesRojas = new List<PocionRoja>();
+    private List<PocionAzul> pocionesAzules = new List<PocionAzul>();
+
+    // Use this for initialization
+    void Start() {
+        Debug.Log("hola mundo inventario");
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void recibirObjeto(Collider2D obj)
+    {
+        switch (obj.tag)
+        {
+           
+            case "pocionRoja":
+                
+                break;
+
+
+            default:
+                break;
+        }
+    }
+
+}
