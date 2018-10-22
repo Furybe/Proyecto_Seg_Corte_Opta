c0af037974d121c34ee33270e1e608bea31cb0b0
JhoanRc <jreyes549@unab.edu.co>
2018-10-21 20:39:43 -0500

    Pruebas puntaje
    pruebas preliminares al sistema de puntuacion por kill y win.

--- Assets/Scripts/Puntuacion/Puntuacion.cs
+++ Assets/Scripts/Puntuacion/Puntuacion.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Puntuacion : MonoBehaviour {
 
 	//atributos - propiedades
-	private double puntos;
+	private int puntos;
+
+	public Text puntaje;
+
+	Jugador jugador = new Jugador();
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +19,20 @@ public class Puntuacion : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (jugador.Salud < 100)
+		{
+			Debug.Log("EL jugador esta perdiendo vida");
+			puntuacion();
+		}
 		
 	}
 	// funcion que se encarga de sumar los puntos obtenidos en la partida.
 	// 1000 puntos por ganar, 100 puntos por matar a otro jugador.
 	private void puntuacion(){
+		if (jugador.Estado == "muerto")
+		{
+			Debug.Log("El jugador " + jugador.tag + " Ha muerto por ");
+		}
 
 	}
 }
