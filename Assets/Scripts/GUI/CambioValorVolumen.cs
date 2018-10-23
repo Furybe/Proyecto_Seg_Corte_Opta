e251acf8a9d35163510358fd3af2d3aa51c2a14b
Camilo <cgarcia104@unab.edu.co>
2018-10-23 00:18:45 -0500

    Modificando GUI
    Agregando Prefabs de Buttons.
    Agregando en escena inicio menu principal, menu de opciones, menu de seleccion de personaje y menu de mapas.
    Implementando soundtrack en la escena de inicio, con su cambio de volumen y pausa de musica.

--- Assets/Scripts/GUI/CambioValorVolumen.cs
+++ Assets/Scripts/GUI/CambioValorVolumen.cs
@@ -2,37 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CambioValorVolumen : MonoBehaviour {
+public class CambioValorVolumen : MonoBehaviour
+{
 
     //  Referencia al componente Audio Source
     private AudioSource soundtrack;
 
     // Variable del Volumen de la musica que va ser modificado
     // Por el Slider 
-    private float VolumenMusica = 1f;
+    private float volumenMusica = 1f;
 
+    // Variable del estado de la musica, true: reproducida; false: en pausa
+    private bool quitarMusica = true;
 
-
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         // Asigan el componente del Audio Source para poder controlarlo
-       soundtrack = GetComponent<AudioSource>();
+        soundtrack = GetComponent<AudioSource>();
 
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         // Los cambios del volumen del Audio Source siendo igual
         // al VolumenMusica
-        soundtrack.volume = VolumenMusica;	
-	}
+        soundtrack.volume = volumenMusica;
+
+    }
 
     // Metodo que es llamado para el objeto slider
     // Este metodo toma al volumen como valor que pasa por el slider
     public void EnviarVolumen(float volumen)
     {
-        VolumenMusica = volumen;
+        volumenMusica = volumen; 
     }
+
+    // Metodo que permite que la musica se pausa por un click 
+    // en en Panel_Music
+    public void QuitarMusica()
+    {
+        if (quitarMusica == true)
+        {
+            soundtrack.Pause();
+            quitarMusica = false;
+        }
+        else
+        {
+            soundtrack.Play();
+            quitarMusica = true;
+        }
+
+
+    }
+
 }
