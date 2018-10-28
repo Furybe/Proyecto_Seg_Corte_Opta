d41c73ab316b00056970d141c4bb2dfc921c8214
Yisus Florez <juniniprogramador@outlook.com>
2018-10-28 00:06:21 -0500

    Implementado prefab fuego fucsia

--- Assets/Scripts/Habilidades/FuegoFucsia.cs
+++ Assets/Scripts/Habilidades/FuegoFucsia.cs
@@ -2,73 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FuegoRojo : Habilidad {
+public class FuegoFucsia : Habilidad {
 
     [SerializeField]
     private Rigidbody2D rb;
     private float velocidadFuego;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
         //inicializando propiedades
-        this.Daño = 50;
+        this.Daño = 35;
         this.velocidadFuego = 6000f;
         rb = GetComponent<Rigidbody2D>();
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		
-	}
 
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
 
     public void lanzar(int idJugador, Transform transformJugador, float direccion)
     {
 
-        
-       
+
         this.IdEjecutor = idJugador;
-        if (direccion==1)
+        if (direccion == 1)
         {
             rb.AddForce(transform.right * 6000f);
-            this.gameObject.transform.position = new Vector3(transformJugador.position.x+3f, transformJugador.position.y + 3f, transformJugador.position.z);
+            this.gameObject.transform.position = new Vector3(transformJugador.position.x + 3f, transformJugador.position.y + 3f, transformJugador.position.z);
 
         }
         else
         {
-            this.gameObject.transform.position = new Vector3(transformJugador.position.x-3f, transformJugador.position.y + 3f, transformJugador.position.z);
+            this.gameObject.transform.position = new Vector3(transformJugador.position.x - 3f, transformJugador.position.y + 3f, transformJugador.position.z);
 
             //agregamos la fuerza al gameobject
-            rb.AddForce(-transform.right* 6000f);
-            
+            rb.AddForce(-transform.right * 6000f);
+
             //giramos el gameObject para que la animación se vea correcta
-            transform.localScale=new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-1, 1, 1);
         }
         Debug.Log("habilidad lanzada");
+        Destroy(gameObject, 3.0f);
     }
 
     void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log(collider.tag);
-        if (collider.tag =="jugador" )
+        if (collider.tag == "jugador")
         {
-            
+            Debug.Log("entró xd jugador");
             if (collider.GetComponent<Jugador>().Id == this.IdEjecutor)
             {
-             
+
             }
             else
             {
-                
+
             }
 
         }
         else
         {
-            Destroy(this.gameObject);
-        }
-       
-    }
 
+        }
+
+    }
 }
