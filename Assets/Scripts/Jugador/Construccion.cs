e7d7374d43dd8f1ca306bf8de68d0ae8e4aff2bb
Yisus Florez <juniniprogramador@outlook.com>
2018-10-29 00:22:10 -0500

    Implementada construcción básica arriba
    Implementada construcción básica arriba

--- Assets/Scripts/Jugador/Construccion.cs
+++ Assets/Scripts/Jugador/Construccion.cs
@@ -1,25 +1,71 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Construccion : MonoBehaviour {
 
+    private SpriteRenderer sprite;
+
+    [SerializeField]
+    private GameObject herramientaArriba;
+
     [SerializeField]
     private GameObject cuadroMadera;
-
+/*
     [SerializeField]
     private GameObject cuadroPiedra;
 
     [SerializeField]
     private GameObject cuadroMetal;
-
+    */
     // Use this for initialization
     void Start () {
-		
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+       
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        
+      if (Input.GetKey(KeyCode.O))
+      {
+          construirArriba();
+      }
+      
+        if (Input.GetKeyDown(KeyCode.O))
+      {
+          construirArriba();
+      }
+      
+        //Debug.Log(herramientaArriba.transform.position.x + "    "+ herramientaArriba.transform.position.y);
+
+    }
+
+    void construirArriba()
+    {
+        Debug.Log("entró construccion");
+        //obtenemos las posiciones de la herramienta arriba para decidir donde poner 
+        //la construcción
+        int posicionX = (int)herramientaArriba.transform.position.x;
+        int posicionY = (int)herramientaArriba.transform.position.y;
+
+        //covertimos a arreglos de char para facilitar el trabajo
+        char[] xArray = posicionX.ToString().ToCharArray();
+        char[] yArray = posicionY.ToString().ToCharArray();
+
+        //cambiamos el último dígito por 5 (para centrar en la cuadrícula)
+        xArray[xArray.Length - 1] = '5';
+        yArray[yArray.Length - 1] = '5';
+
+      
+        //obtenemos el nuevo número 
+        posicionX = Int32.Parse(new string(xArray));
+        posicionY = Int32.Parse(new string(yArray));
+
+        Instantiate(cuadroMadera, new Vector3(posicionX, posicionY, 0),Quaternion.identity);
+
+    }
+
+
 }
