6f65cb3be6f4a6c541977df840affac622fb8c43
Furybe <curibe895@unab.edu.co>
2018-10-29 00:20:40 -0500

    Diseño principal hub de objetos
    Parte grafica de la barra de objetos.

---
+++ Assets/Scripts/Jugador/Construccion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Construccion : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject cuadroMadera;
+
+    [SerializeField]
+    private GameObject cuadroPiedra;
+
+    [SerializeField]
+    private GameObject cuadroMetal;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
