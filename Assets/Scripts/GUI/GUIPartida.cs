21cb4d44e0d81a04c2d95f1929f039bd47b0ccc8
juniniv3 <juniniprogramador@outlook.com>
2018-10-30 12:53:48 -0500

    Agregada navegación para presentación

---
+++ Assets/Scripts/GUI/GUIPartida.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIPartida : MonoBehaviour {
+
+
+
+    [SerializeField]
+    private Button boton;
+
+
+    // Use this for initialization
+    void Start () {
+
+        Button botonIniciarPartida = boton.GetComponent<Button>();
+        botonIniciarPartida.onClick.AddListener(iniciar);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void iniciar()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Multijugador");
+    }
+}
