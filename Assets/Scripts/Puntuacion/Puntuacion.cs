088579ab91ab44389b4a4b6c42395b07f0ce52a2
JhoanRc <jreyes549@unab.edu.co>
2018-10-30 10:26:20 -0500

    Creación prefab scoreboard
    Implementacion del prefab de scoreboard y avances en puntaje.

--- Assets/Scripts/Puntuacion/Puntuacion.cs
+++ Assets/Scripts/Puntuacion/Puntuacion.cs
@@ -12,6 +12,8 @@ public class Puntuacion : MonoBehaviour {
 
 	public Jugador jugador = new Jugador();
 	
+	public GameObject scoreboard;
+
 	// Update is called once per frame
 	void Update () {
 		/*
@@ -22,14 +24,37 @@ public class Puntuacion : MonoBehaviour {
 			puntaje.text = jugador.Salud.ToString("0");
 		}
 		*/
+		//ver scoreboard
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            scoreBoard();
+        }
+		if (Input.GetKeyUp(KeyCode.Tab))
+		{
+			scoreboard.SetActive(false);
+		}
+		if (Input.GetKeyUp(KeyCode.PageUp))
+		{
+			puntuacion();
+		}
 	}
 	// funcion que se encarga de sumar los puntos obtenidos en la partida.
 	// 1000 puntos por ganar, 100 puntos por matar a otro jugador.
 	private void puntuacion(){
 		if (jugador.Estado == "vivo")
 		{
-			Debug.Log("El jugador " + jugador.Id + " ha ganado 100 puntos");
+			Debug.Log("El jugador " + jugador.Id + " ha ganado 50 puntos por estar vivo");
+			puntos = puntos  + 50;
+			puntaje.text = puntos.ToString();
 		}
+
+	}
+	// funcion que permite ver la puntuacion ingame
+	public void scoreBoard(){
+
+		scoreboard.SetActive(true);
+
+		//actualizar scoreboard con la lista de jugadores
 
 	}
 }
